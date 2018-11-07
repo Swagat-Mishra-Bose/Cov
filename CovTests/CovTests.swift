d5610d4484a9ab7a8a2cdb2d3614434ee36e037b
Swagat Mishra <swagat_mishra@bose.com>
2018-11-07 13:46:00 -0500

    secondreponowtestfail

--- CovTests/CovTests.swift
+++ CovTests/CovTests.swift
@@ -23,6 +23,10 @@ class CovTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testNow() {
+        XCTFail("Failed in testNow()")
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
